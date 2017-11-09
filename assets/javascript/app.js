90ebc06ee5bb55956485d62fa962dc3df3377367
wesdriver45 <wesdriver45@gmail.com>
2017-11-08 20:34:47 -0500

    firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+// firebase
+// <script src="https://www.gstatic.com/firebasejs/4.6.1/firebase.js"></script>
+// <script>
+//   // Initialize Firebase
+//   var config = {
+//     apiKey: "AIzaSyCRiEoXb5A2kXcabewUgyRpzqSoIAvkBj8",
+//     authDomain: "train-scheduler-24889.firebaseapp.com",
+//     databaseURL: "https://train-scheduler-24889.firebaseio.com",
+//     projectId: "train-scheduler-24889",
+//     storageBucket: "",
+//     messagingSenderId: "974558203088"
+//   };
+//   firebase.initializeApp(config);
+// </script>
